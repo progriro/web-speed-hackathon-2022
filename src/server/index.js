e40b02019f866d04a7863f50df374e22a288665a
shinya fukimbara <progriro@gmail.com>
2022-08-04 20:17:07 +0900

    perf: pino-pretty を削除

--- src/server/index.js
+++ src/server/index.js
@@ -9,18 +9,7 @@ import { spaRoute } from "./routes/spa.js";
 import { createConnection } from "./typeorm/connection.js";
 import { initialize } from "./typeorm/initialize.js";
 
-const IS_PRODUCTION = process.env.NODE_ENV === "production";
-
-const server = fastify({
-  logger: IS_PRODUCTION
-    ? false
-    : {
-        prettyPrint: {
-          ignore: "pid,hostname",
-          translateTime: "SYS:HH:MM:ss",
-        },
-      },
-});
+const server = fastify({ logger: process.env.NODE_ENV === "development" });
 server.register(fastifySensible);
 
 server.addHook("onRequest", async (req, res) => {
