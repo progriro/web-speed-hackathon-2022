480b34c6a5739ea14c5f7c950f2a96efa292ceb1
shinya fukimbara <progriro@gmail.com>
2022-08-05 04:47:43 +0900

    perf: cloudflare での静的ファイルのキャッシュを有効化

--- src/server/routes/spa.js
+++ src/server/routes/spa.js
@@ -15,6 +15,9 @@ export const spaRoute = async (fastify) => {
   });
 
   fastify.get("*", (_req, reply) => {
+    reply.header("CDN-Cache-Control", "max-age=60");
+    reply.header("Cache-Control", "public, max-age=0");
+
     return reply.sendFile("index.html", join(__dirname, "public"));
   });
 };
